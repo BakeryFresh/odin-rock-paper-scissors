838e88ff3c004d7aafe41404676fb8c901a21620
BakeryFresh <shamanmclean@gmail.com>
2023-04-27 14:05:04 +0200

    Add dark mode

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-
 //<!-- let computerMove;
 //let computerMoveOutput; -->
 
 //computer chooses a number randomly between 1-3
 //if 1 rock if 2 paper if 3 scissors
 //once move is determined return string
+
+const playerButtons = document.querySelectorAll(".player-action");
+const bgSwitch = document.querySelector("#bgSwitch");
+
+bgSwitch.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+  
+  playerButtons.forEach((button) => {
+    button.classList.toggle("dark");
+  });
+});
 
 function getComputerChoice() {
   computerMove = Math.floor(Math.random() * (3 - 1 + 1) + 1);
@@ -46,63 +56,63 @@ function game() {
   let computerScore = 0;
 
   //only allows five non-draw games to be played.
-//   while (playerScore + computerScore < 5) {
-//     alert(
-//       "Current Score: Player: " + playerScore + " Computer: " + computerScore
-//     );
-    //computer choice is automated via func getComputerChoice which gives a random 1-3 and returns RPS.
-    const computerSelection = getComputerChoice();
-    const playerSelection = getPlayerChoice();
+  //   while (playerScore + computerScore < 5) {
+  //     alert(
+  //       "Current Score: Player: " + playerScore + " Computer: " + computerScore
+  //     );
+  //computer choice is automated via func getComputerChoice which gives a random 1-3 and returns RPS.
+  const computerSelection = getComputerChoice();
+  const playerSelection = getPlayerChoice();
 
-    playRound(playerSelection, computerSelection);
-    //plays a single round of RPS. Winner depends on computer vs human choice.
-    function playRound(playerSelection, computerSelection) {
-      if (playerSelection === "rock") {
-        switch (computerSelection) {
-          case "rock":
-            alert("It was a draw, try again!");
-            break;
+  playRound(playerSelection, computerSelection);
+  //plays a single round of RPS. Winner depends on computer vs human choice.
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+      switch (computerSelection) {
+        case "rock":
+          alert("It was a draw, try again!");
+          break;
 
-          case "paper":
-            return computerScore++;
+        case "paper":
+          return computerScore++;
 
-            break;
+          break;
 
-          case "scissors":
-            return playerScore++;
-            break;
-        }
-      } else if (playerSelection === "paper") {
-        switch (computerSelection) {
-          case "rock":
-            return playerScore++;
-            break;
+        case "scissors":
+          return playerScore++;
+          break;
+      }
+    } else if (playerSelection === "paper") {
+      switch (computerSelection) {
+        case "rock":
+          return playerScore++;
+          break;
 
-          case "paper":
-            alert("It was a draw, try again!");
-            break;
+        case "paper":
+          alert("It was a draw, try again!");
+          break;
 
-          case "scissors":
-            return computerScore++;
-            break;
-        }
-      } else if (playerSelection === "scissors") {
-        switch (computerSelection) {
-          case "rock":
-            return computerScore++;
-            break;
+        case "scissors":
+          return computerScore++;
+          break;
+      }
+    } else if (playerSelection === "scissors") {
+      switch (computerSelection) {
+        case "rock":
+          return computerScore++;
+          break;
 
-          case "paper":
-            return playerScore++;
-            break;
+        case "paper":
+          return playerScore++;
+          break;
 
-          case "scissors":
-            alert("It was a draw, try again!");
-            break;
-        }
+        case "scissors":
+          alert("It was a draw, try again!");
+          break;
       }
     }
-//   }
+  }
+  //   }
 
   //announce winner to the console
   if (playerScore > computerScore) {
