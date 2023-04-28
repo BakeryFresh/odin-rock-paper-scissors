10ad84932a607b9edcfa0cdc282ccbeadef2805f
BakeryFresh <shamanmclean@gmail.com>
2023-04-28 13:14:22 +0200

    Add images for computer choice. Add JS to change computer images

--- script.js
+++ script.js
@@ -1,144 +1,155 @@
-//<!-- let computerMove;
-//let computerMoveOutput; -->
-
-//computer chooses a number randomly between 1-3
-//if 1 rock if 2 paper if 3 scissors
-//once move is determined return string
 
 const playerButtons = document.querySelectorAll(".player-action");
 const bgSwitch = document.querySelector("#bgSwitch");
+const playerScoreboard = document.querySelector("#playerScoreboard");
+const computerScoreboard = document.querySelector("#computerScoreboard");
+let computerChoiceImage = document.getElementById('computerChoiceImage');
+
+let playerScore = 0;
+let computerScore = 0;
 
 //toggle light or dark mode
 bgSwitch.addEventListener("change", () => {
   document.body.classList.toggle("dark");
-  
+
   playerButtons.forEach((button) => {
     button.classList.toggle("dark");
   });
 });
 
-//listen for each of the buttons and declare the players choice for the one chosen.
-playerButtons.forEach((playerButtons) =>{
-    playerButtons.addEventListener('click', () => {
-      
-      playerChoice = playerButtons.id;
-      console.log(playerChoice)
-    });
+//listen for each of the buttons and declare the players selection for the one chosen
+playerButtons.forEach((playerButtons) => {
+  playerButtons.addEventListener("click", () => {
+    playerSelection = playerButtons.id;
+
+    playRound(playerSelection);
+  });
 });
 
-
+//computer chooses a number randomly between 1-3
+//if 1 rock if 2 paper if 3 scissors once move is determined return string
 function getComputerChoice() {
   computerMove = Math.floor(Math.random() * (3 - 1 + 1) + 1);
 
   if (computerMove === 1) {
     computerMoveOutput = "rock";
+    console.log(computerChoiceImage);
+    computerChoiceImage.src = "./images/misc-pet-rock.svg";
     return computerMoveOutput;
   } else if (computerMove === 2) {
     computerMoveOutput = "paper";
+    console.log(computerChoiceImage);
+    computerChoiceImage.src = "./images/loose_leaf_paper.svg";
+
     return computerMoveOutput;
   } else {
     computerMoveOutput = "scissors";
+    console.log(computerChoiceImage);
+    computerChoiceImage.src = "./images/Machovka-Scissors-1.svg";
+
     return computerMoveOutput;
   }
 }
 
 //function to let the player make a game choice. prevents the user from entering non-sanctioned values
-// function getPlayerChoice() {
-//   playerChoice = prompt("What is your choice? (Rock, Paper, Scissors): ");
-//   if (playerChoice != "" && playerChoice != null) {
-//     if (
-//       playerChoice.toLowerCase() !== "rock" &&
-//       playerChoice.toLowerCase() !== "scissors" &&
-//       playerChoice.toLowerCase() !== "paper"
-//     ) {
-//       alert("Please only enter 'Rock, Paper, or Scissors'");
-//     } else {
-//       console.log(playerChoice + " success");
-//       return playerChoice.toLowerCase();
-//     }
-//   } else {
-//     alert("You've cancelled the operation");
-//   }
-// }
+function getPlayerChoice() {
+  // playerChoice = prompt("What is your choice? (Rock, Paper, Scissors): ");
+  // if (playerChoice != "" && playerChoice != null) {
+  //   if (
+  //     playerChoice.toLowerCase() !== "rock" &&
+  //     playerChoice.toLowerCase() !== "scissors" &&
+  //     playerChoice.toLowerCase() !== "paper"
+  //   ) {
+  //     alert("Please only enter 'Rock, Paper, or Scissors'");
+  //   } else {
+  //     console.log(playerChoice + " success");
+  //     return playerChoice.toLowerCase();
+  //   }
+  // } else {
+  //   alert("You've cancelled the operation");
+  // }
+}
 
-//function to start the game, it will go to a best of 5
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+//plays a single round of RPS. Winner depends on computer vs human choice.
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
 
-  //only allows five non-draw games to be played.
-  //   while (playerScore + computerScore < 5) {
-  //     alert(
-  //       "Current Score: Player: " + playerScore + " Computer: " + computerScore
-  //     );
-  //computer choice is automated via func getComputerChoice which gives a random 1-3 and returns RPS.
-  const computerSelection = getComputerChoice();
-  const playerSelection = getPlayerChoice();
+  console.log("Player Selection function started");
+  console.log("Play Sel: " + playerSelection);
+  console.log("Comp Sel: " + computerSelection);
 
-  playRound(playerSelection, computerSelection);
-  //plays a single round of RPS. Winner depends on computer vs human choice.
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock") {
-      switch (computerSelection) {
-        case "rock":
-          alert("It was a draw, try again!");
-          break;
+  if (playerSelection === "rock") {
+    switch (computerSelection) {
+      case "rock":
+        console.log("draw");
+        break;
 
-        case "paper":
-          return computerScore++;
+      case "paper":
+        computerScore++;
 
-          break;
+        break;
 
-        case "scissors":
-          return playerScore++;
-          break;
-      }
-    } else if (playerSelection === "paper") {
-      switch (computerSelection) {
-        case "rock":
-          return playerScore++;
-          break;
+      case "scissors":
+        playerScore++;
+        break;
+    }
+  } else if (playerSelection === "paper") {
+    switch (computerSelection) {
+      case "rock":
+        playerScore++;
+        break;
 
-        case "paper":
-          alert("It was a draw, try again!");
-          break;
+      case "paper":
+        console.log("draw");
+        break;
 
-        case "scissors":
-          return computerScore++;
-          break;
-      }
-    } else if (playerSelection === "scissors") {
-      switch (computerSelection) {
-        case "rock":
-          return computerScore++;
-          break;
+      case "scissors":
+        computerScore++;
+        break;
+    }
+  } else if (playerSelection === "scissors") {
+    switch (computerSelection) {
+      case "rock":
+        computerScore++;
+        break;
 
-        case "paper":
-          return playerScore++;
-          break;
+      case "paper":
+        playerScore++;
+        break;
 
-        case "scissors":
-          alert("It was a draw, try again!");
-          break;
-      }
+      case "scissors":
+        console.log("draw");
+        break;
     }
   }
-  //   }
 
   //announce winner to the console
-  if (playerScore > computerScore) {
-    alert(
-      "You win! Your Score: " +
-        playerScore +
-        " Computer Score: " +
-        computerScore
-    );
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore > computerScore) {
+      alert(
+        "You win! Your Score: " +
+          playerScore +
+          " Computer Score: " +
+          computerScore
+      );
+    } else {
+      alert(
+        "You Lose! Your Score: " +
+          playerScore +
+          " Computer Score: " +
+          computerScore
+      );
+    }
+    console.log("PS: " + playerScore);
+    playerScoreboard.textContent = playerScore;
+    console.log("CS: " + computerScore);
+    computerScoreboard.textContent = computerScore;
+
   } else {
-    alert(
-      "You Lose! Your Score: " +
-        playerScore +
-        " Computer Score: " +
-        computerScore
-    );
-  }
+
+  console.log("PS: " + playerScore);
+  playerScoreboard.textContent = playerScore;
+  console.log("CS: " + computerScore);
+  computerScoreboard.textContent = computerScore;
+}
 }
